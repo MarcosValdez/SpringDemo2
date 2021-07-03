850c43d24fb0483f871baabaea2975b485c1d8a0
AlexisRM0511 <alexis.rojas1@unmsm.edu.pe>
2021-07-02 22:30:39 -0500

    Editorial y Comprador

--- src/main/java/com/example/demo3/application/inteface/IEditorialServiceJPA.java
+++ src/main/java/com/example/demo3/application/inteface/IEditorialServiceJPA.java
@@ -1,9 +1,13 @@
 package com.example.demo3.application.inteface;
 
 import com.example.demo3.application.entity.EditorialJPA;
+import com.example.demo3.application.entity.LibroJPA;
 
 import java.util.List;
 
 public interface IEditorialServiceJPA {
     List<EditorialJPA> list();
+    EditorialJPA getById(Integer id);
+    EditorialJPA save(EditorialJPA editorial);
+    void delete(Integer id);
 }
--- src/main/java/com/example/demo3/controller/CompradorControllerJPA.java
+++ src/main/java/com/example/demo3/controller/CompradorControllerJPA.java
@@ -1,16 +1,13 @@
 package com.example.demo3.controller;
 
 import com.example.demo3.application.entity.CompradorJPA;
-import com.example.demo3.application.entity.LibroJPA;
 import com.example.demo3.application.service.CompradorServiceJPA;
-import com.example.demo3.application.service.LibroServiceJPA;
+import com.example.demo3.infrastructure.common.ServiceResult;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
 
 import java.util.List;
 
@@ -26,6 +23,52 @@ public class CompradorControllerJPA {
         try {
             return new ResponseEntity(compradorServiceJPA.list(), HttpStatus.OK);
         } catch (Exception ex) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/list/{id}")
+    public ResponseEntity<List<CompradorJPA>> listById(@PathVariable Integer id) {
+        try {
+            return new ResponseEntity(compradorServiceJPA.getById(id), HttpStatus.OK);
+        } catch (Exception ex) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<ServiceResult> create(@RequestBody String comprador) {
+
+        ServiceResult serviceResult = new ServiceResult();
+        try {
+            CompradorJPA a = new ObjectMapper().readValue(comprador, CompradorJPA.class);
+            compradorServiceJPA.save(a);
+            serviceResult.setMessage("comprador registrado");
+            serviceResult.setData(null);
+            return new ResponseEntity<>(serviceResult, HttpStatus.CREATED);
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            serviceResult.setError(ex.getMessage());
+            serviceResult.setSuccess(false);
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<ServiceResult> delete(@PathVariable Integer id) {
+        ServiceResult serviceResult = new ServiceResult();
+
+        try {
+            compradorServiceJPA.delete(id);
+            serviceResult.setMessage("Comprador eliminado");
+            serviceResult.setData(null);
+            return new ResponseEntity<>(serviceResult, HttpStatus.CREATED);
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            serviceResult.setError(ex.getMessage());
+            serviceResult.setSuccess(false);
             return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
