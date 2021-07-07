9a89c0b3ff36cd62e5333bbcc07a8e6694854e7e
Eduardo <eduardo.navarro2@unmsm.edu.pe>
2021-07-07 18:36:14 -0500

    venta

--- src/main/java/com/example/demo3/application/service/VentaServiceJPA.java
+++ src/main/java/com/example/demo3/application/service/VentaServiceJPA.java
@@ -1,13 +1,17 @@
 package com.example.demo3.application.service;
 
+import com.example.demo3.application.entity.CompradorJPA;
 import com.example.demo3.application.entity.LibroJPA;
 import com.example.demo3.application.entity.VentaJPA;
+import com.example.demo3.application.inteface.ICompradorServiceJPA;
+import com.example.demo3.application.inteface.ILibroServiceJPA;
 import com.example.demo3.application.inteface.IVentaServiceJPA;
 import com.example.demo3.infrastructure.mapper.LibroRepository;
 import com.example.demo3.infrastructure.mapper.VentaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service("ventaServiceJPA")
@@ -15,8 +19,15 @@ public class VentaServiceJPA implements IVentaServiceJPA {
     @Autowired
     VentaRepository ventaRepository;
 
+    @Autowired
+    ICompradorServiceJPA compradorServiceJPA;
+
+    @Autowired
+    ILibroServiceJPA libroServiceJPA;
+
     @Override
     public List<VentaJPA> list() {
+
         return (List<VentaJPA>) ventaRepository.findAll();
     }
 
@@ -26,11 +37,17 @@ public class VentaServiceJPA implements IVentaServiceJPA {
     }
 
     @Override
-    public VentaJPA save(VentaJPA venta) {return ventaRepository.save(venta);}
+    public VentaJPA save(VentaJPA venta) {
+        CompradorJPA compradorJPA = compradorServiceJPA.save(venta.getComprador());
+        venta.setComprador(compradorJPA);
+        venta.setFecha(LocalDateTime.now());
+        libroServiceJPA.vender(venta.getLibro().getLibroId());
+        return ventaRepository.save(venta);
+    }
 
     @Override
     public void delete(Integer id) {
-        VentaJPA venta =  new VentaJPA();
+        VentaJPA venta = new VentaJPA();
         venta.setVentaId(id);
         ventaRepository.delete(venta);
     }
